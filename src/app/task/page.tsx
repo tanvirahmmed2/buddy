d68e5a5ef37566c482b4453c96a85aa492837d46
Tanvir Ahmmed <tanvir004006@gmail.com>
2026-01-16 07:09:37 +0600

    task form

--- src/app/task/page.tsx
+++ src/app/task/page.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+'use client'
+
+import { Context } from "@/component/context/Context"
+import AddTaskForm from "@/component/forms/AddTaskForm"
+import Link from "next/link"
+import { useContext } from "react"
 
 const TaskPage = () => {
+  const { tasks } = useContext(Context)
   return (
-    <div>
-      
+    <div className='w-full flex flex-col gap-4 min-h-screen p-4'>
+
+      <AddTaskForm />
+
+      {
+        tasks === null ? <div className="w-full h-auto flex items-center justify-center p-1">
+          <p>No Task Found</p>
+        </div> :
+          <div>
+            {
+              tasks.map((task: any) => (
+                <div key={task._id}>
+                  <Link href={`/task/${task._id}`}>{task.title}</Link>
+                  <div>
+                    <p>{task.status}</p>
+                    <p>Deadline :{task.deadline.slice(0, 10)}</p>
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+      }
     </div>
   )
 }
---
+++ src/component/forms/AddTaskForm.tsx
@@ -0,0 +1,65 @@
+'use client'
+
+import axios from "axios"
+import { useContext, useState } from "react"
+import { Context } from "../context/Context"
+
+const AddTaskForm = () => {
+    const {fetchTask}= useContext(Context)
+    const [formData, setFormData] = useState({
+        title: '',
+        description: '',
+        priority: '',
+        deadline: ''
+    })
+
+    const changeHandler = (e: any) => {
+        const { name, value } = e.target
+        setFormData((prev) => ({ ...prev, [name]: value }))
+    }
+
+    const submitTask = async (e: any) => {
+        e.preventDefault()
+        try {
+            const response = await axios.post('/api/task', formData, { withCredentials: true })
+            alert(response.data.message)
+            fetchTask()
+        } catch (error: any) {
+            console.log(error)
+            alert(error?.response?.data?.message || "Failed to add task")
+
+        }
+    }
+    return (
+        <form onSubmit={submitTask}>
+            <div className="w-full flex flex-col items-center justify-center gap-1">
+                <label htmlFor="title">Title</label>
+                <input type="text" required name="title" id="title" value={formData.title} onChange={changeHandler} className="w-full px-3 p-1 border outline-none" />
+            </div>
+            <div className="w-full flex flex-col md:flex-row items-center justify-center gap-2">
+
+                <div className="w-full flex flex-col gap-1">
+                    <label htmlFor="priority">Priority</label>
+                    <select name="priority" id="priority" value={formData.priority} required onChange={changeHandler} className="w-full px-3 p-1 border outline-none">
+                        <option value="low">Low</option>
+                        <option value="medium">Medium</option>
+                        <option value="high">High</option>
+                    </select>
+                </div>
+
+                <div className="w-full flex flex-col gap-1">
+                    <label htmlFor="deadline">Deadline</label>
+                    <input type="date" required name="deadline" id="deadline" value={formData.deadline} onChange={changeHandler} className="w-full px-3 p-1 border outline-none" />
+                </div>
+            </div>
+
+            <div className="w-full flex flex-col gap-1">
+                <label htmlFor="description">Description</label>
+                <textarea required name="description" id="description" value={formData.description} onChange={changeHandler} className="w-full px-3 p-1 border outline-none" />
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+    )
+}
+
+export default AddTaskForm
