7cef36da623b99666ae12b5eb1a7ec7fb2be2ccd
Tanvir Ahmmed <tanvir004006@gmail.com>
2026-01-18 11:36:58 +0600

    ui

--- src/component/pages/Intro.tsx
+++ src/component/pages/Intro.tsx
@@ -1,9 +1,30 @@
+'use client'
 import React from 'react'
+import Typewriter from 'typewriter-effect'
 
 const Intro = () => {
   return (
-    <div>
-      
+    <div className='w-full flex min-h-150 p-2 flex-col md:flex-row items-center justify-center bg-emerald-300'>
+      <div className='w-full flex flex-col items-center justify-center gap-4'>
+        <p className='w-full text-center uppercase italic text-xl'>Met Your Best Task Manager</p>
+        <div className='text-4xl md:text-8xl font-semibold  drop-shadow-2xl'>
+          <Typewriter
+            options={{
+              strings: 'Buddy',
+              autoStart: true,
+              loop: true,
+              deleteSpeed: 50,
+              delay: 100,
+              cursor: '.'
+            }}
+          />
+        </div>
+        <p className='w-full text-center uppercase italic text-xl'>Make Your Day Productive</p>
+      </div>
+      <div>
+
+      </div>
+
     </div>
   )
 }
